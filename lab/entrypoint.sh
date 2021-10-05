98a43b4329f934a6b18e36c957a8ebc272fb6d3d
aki <030bug@gmail.com>
2021-10-05 21:58:30 +0100

    adding support for dockernel

--- lab/entrypoint.sh
+++ lab/entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 if [[ $1 == 'jupyter' ]]; then
     jupyter lab --ip=0.0.0.0 --allow-root --no-browser 
-elif [[ $1 == 'python' ]]; then
-    python3
 else
     $1
 fi
--- rstudio/entrypoint.sh
+++ rstudio/entrypoint.sh
@@ -3,8 +3,6 @@ set -e
 
 if [[ $1 == 'rstudio' ]]; then
     /usr/lib/rstudio-server/bin/rserver --server-daemonize=0
-elif [[ $1 == 'r' ]]; then
-    R
 else
     $1
 fi
